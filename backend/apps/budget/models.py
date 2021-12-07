7921db88851999d4fa529bdcff9da0a4c76be89a
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2021-12-07 14:55:04 +0100

    Add budget model +migrations

--- backend/apps/budget/models.py
+++ backend/apps/budget/models.py
@@ -1,3 +1,31 @@
-# from django.db import models
+from apps.users.models import User
+from django.db import models
 
-# Create your models here.
+
+class TimeStamps(models.Model):
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseMixin(models.Model):
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Budget(BaseMixin):
+    creator = models.ForeignKey(User, related_name="creator", on_delete=models.PROTECT)
+    participants = models.ManyToManyField(User, related_name="participants")
+    name = models.CharField(max_length=30)
+    content = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return f"Name: {self.name}, Creator {self.creator}"
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
