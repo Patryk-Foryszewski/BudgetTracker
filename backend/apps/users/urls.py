10f7755d03f9515ef690dbf734d7916499438582
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2021-12-07 14:04:03 +0100

    Chage entry path for auth urls

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import logout
 from . views import UsersList, UserProfile
 
 urlpatterns = [
+    path("", include("dj_rest_auth.urls")),
     path("logout/", logout, {'next_page': '/'}, name='logout'),
-    path("auth", include("dj_rest_auth.urls")),
     path("registration/", include("dj_rest_auth.registration.urls")),
     path('users/profile', UserProfile.as_view(), name='user_profile'),
     path('users/list', UsersList.as_view(), name='users_list')
