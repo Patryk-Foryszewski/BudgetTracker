0427d25f0e2cc4ada537114e295fa043e3da8f00
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2022-01-11 08:37:48 +0100

    Add test for FriendRemove view

--- backend/apps/friends/tests/test_views.py
+++ backend/apps/friends/tests/test_views.py
@@ -7,17 +7,22 @@ from rest_framework import status
 from rest_framework.test import force_authenticate
 
 from ..models import Friends
-from ..views import FriendsAdd, FriendsList
+from ..views import FriendsAdd, FriendsList, FriendsRemove
 
 
 class AddFriends(TestCase):
     @classmethod
-    def setUpTestData(cls) -> None:
+    def setUp(cls) -> None:
         cls.user = UserFactory()
         cls.friend_1 = UserFactory()
         cls.friend_2 = UserFactory()
 
-    def test_add_two_friends(self):
+    def test_unauthenticated_user(self):
+        request = request_factory.patch("/")
+        response = FriendsAdd.as_view()(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_add_two_friends_one_by_one(self):
         data = json.dumps({"friends_list": [self.friend_1.pk]})
         request = request_factory.patch("/", data=data, content_type="application/json")
         force_authenticate(request, user=self.user)
@@ -30,6 +35,37 @@ class AddFriends(TestCase):
         response = FriendsAdd.as_view()(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(2, len(self.user.friends.friends_list.all()))
+
+    def test_add_two_friends_at_a_time(self):
+        data = json.dumps({"friends_list": [self.friend_1.pk, self.friend_2.pk]})
+        request = request_factory.patch("/", data=data, content_type="application/json")
+        force_authenticate(request, user=self.user)
+        response = FriendsAdd.as_view()(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(2, len(self.user.friends.friends_list.all()))
+
+
+class RemoveFriend(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.user = UserFactory()
+        cls.friend_1 = UserFactory()
+        cls.friend_2 = UserFactory()
+        cls.friends = Friends.objects.create(user=cls.user)
+        cls.friends.friends_list.set([cls.friend_1, cls.friend_2])
+
+    def test_unauthenticated_user(self):
+        request = request_factory.patch("/")
+        response = FriendsRemove.as_view()(request)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_remove_friend(self):
+        data = json.dumps({"friends_list": [self.friend_1.pk]})
+        request = request_factory.patch("/", data=data, content_type="application/json")
+        force_authenticate(request, user=self.user)
+        response = FriendsRemove.as_view()(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(1, len(self.friends.friends_list.all()))
 
 
 class ListFriends(TestCase):
