16190101b57d1d10a028289cd86495937e810fdc
Patryk-Foryszewski <patryk.foryszewski@gmail.com>
2022-01-17 16:08:33 +0100

    Add path to RemoveParticipants

--- backend/apps/budget/urls.py
+++ backend/apps/budget/urls.py
@@ -1,10 +1,21 @@
 from django.urls import path
 
-from .views import BudgetCreate, BudgetDetail, BudgetList, BudgetUpdate
+from .views import (
+    BudgetCreate,
+    BudgetDetail,
+    BudgetList,
+    BudgetRemoveParticipants,
+    BudgetUpdate,
+)
 
 urlpatterns = [
     path("create/", BudgetCreate.as_view(), name="create"),
     path("update/", BudgetUpdate.as_view(), name="update"),
     path("list/", BudgetList.as_view(), name="list"),
     path("detail/", BudgetDetail.as_view(), name="detail"),
+    path(
+        "remove_participants/",
+        BudgetRemoveParticipants.as_view(),
+        name="remove_participants",
+    ),
 ]
